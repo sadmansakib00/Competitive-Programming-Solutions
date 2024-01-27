07de306b6827c84d87f60aa35c3b02b9c65777ac
Sadman Sakib <sadman.sakib@brainstation-23.com>
2024-01-28 02:15:05 +0600

    Adds Beautiful Matrix of Codeforces

---
+++ C++/CodeForces/BeautifulMatrix.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#pragma gcc optimize("O3")
+//#pragma gcc optimize("unroll-loops")
+#ifdef EVAH
+    #define _WIN32_WINNT 0x0500
+    //#include<windows.h>
+#endif
+#define openFile(n) ShellExecute(nullptr,"open",n,nullptr,nullptr,SW_SHOWNORMAL);
+#define closeP(n) system((string("taskkill /im ")+n+" /f").c_str());
+#define closeConsole PostMessage(GetConsoleWindow(), WM_CLOSE, 0, 0);
+#define opf "output.txt"
+#define ipf "input.txt"
+#define mem(n,x) memset(n,x,sizeof(n));
+#define sf scanf
+#define pf printf
+#define ff first
+#define ss second
+#define pb push_back
+#define PII pair<int,int>
+#define For(x,n) for(int i=x; i<n; i++)
+#define stv v+2*(mid-tl+1)
+#define LL long long int
+#define N 100000000
+ 
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    #ifdef EVAH
+        //freopen(ipf,"r",stdin);
+        //openFile(ipf);
+        //closeConsole;
+        //freopen(opf,"w",stdout);
+    #endif
+
+    int temp, ans = 0;
+    for(int i=1; i<6; i++) {
+        for(int j=1; j<6; j++) {
+            cin >> temp;
+            if(temp == 1) {
+                ans = abs(3-i)+abs(3-j);
+            }
+        }
+    }
+    cout << ans << "\n";
+
+
+ 
+    #ifdef EVAH
+        //openFile(opf);
+        //Sleep(3000);
+        //closeP("notepad.exe");
+        //closeConsole;
+    #endif
+}
